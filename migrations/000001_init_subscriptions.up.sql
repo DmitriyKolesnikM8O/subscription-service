0f9bc19d97b3f34b2477297b4372047be88dfcb3
Dmitriy <kolesnik982011@gmail.com>
2025-07-17 23:17:18 +0300

    add migrations

---
+++ migrations/000001_init_subscriptions.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE subscriptions (
+    id UUID PRIMARY KEY,
+    service_name TEXT NOT NULL,
+    price INTEGER NOT NULL CHECK (price > 0),
+    user_id UUID NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE INDEX idx_subscriptions_user_id ON subscriptions(user_id);
+CREATE INDEX idx_subscriptions_service ON subscriptions(service_name);
